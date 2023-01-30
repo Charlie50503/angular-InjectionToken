c61ca9767523ac5448bddefe1177d0dc7bf266d0
charlie50503 <cate50503@gmail.com>
2023-01-30 21:05:37 +0800

    feat: 在 module 使用 deps
    告訴 angular 我的 useFactory 有依賴於其他DI,來從外部改變進到組件後的效果

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { MessageService } from './message.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClient} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewMessageService } from './new-message.service';
@@ -19,9 +19,10 @@ import { CustomComponent } from './custom/custom.component';
   providers: [
     {
       provide: MessageService,
-      useFactory: () => {
+      useFactory: (httpClient:HttpClient) => {
         return new Date().getFullYear() >= 2024 ? new NewMessageService() : new MessageService()
-      }
+      },
+      deps:[HttpClient]
     }
   ],
   bootstrap: [AppComponent]
