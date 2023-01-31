96a99b25dea20c2184d4067b6a387b44a3de44ce
charlie50503 <cate50503@gmail.com>
2023-01-31 22:13:32 +0800

    feat: 不透過 module 注入

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, Inject, InjectionToken, Optional } from '@angular/core';
 // import { MessageService } from './message.service';
 
 export const messageInjectToken = new InjectionToken<Message>("Message Token");
-export const messageInjectToken = new InjectionToken<string>("Message Token");
+export const MY_TOKEN = new InjectionToken<string>('My Token');
 export class Message {
   message!:string;
   user!:string
@@ -11,12 +11,13 @@ export class Message {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [{ provide: MY_TOKEN, useValue: 'My Value' }]
 })
 export class AppComponent {
   constructor(
     // public messageService:MessageService,
-    @Inject("messageInjectToken") @Optional() public message:string
+    @Inject(MY_TOKEN) public value: string,
     @Inject(messageInjectToken)  public message:Message
   ){
 
